48285af230aaed94cbaa9f60df9e9d4e5937ee59
Erick <erickfrias8006@gmail.com>
2024-03-26 09:04:32 -0300

    Primeiro Comit

---
+++ PouBeLike/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PouBeLike;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PouBeLike/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PouBeLike;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
