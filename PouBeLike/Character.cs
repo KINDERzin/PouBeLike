a08e6efc45c3be60838bbc067558a2e685ed4624
Erick <erickfrias8006@gmail.com>
2024-03-28 11:55:00 -0300

    Terceiro COMIT

---
+++ PouBeLike/Character.cs
@@ -0,0 +1,22 @@
+namespace PouBeLike;
+
+public class Character
+{
+    double Sede;
+    double Fome;
+    double Felicidade;
+
+//------------------------------------------------------------
+
+    public Character()
+    {
+        Sede = 0;
+        Fome = 0;
+        Felicidade = 0;
+    }
+
+//------------------------------------------------------------
+
+    
+
+}
--- PouBeLike/MainPage.xaml.cs
+++ PouBeLike/MainPage.xaml.cs
@@ -7,7 +7,33 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+	}
 
+//------------------------------------------------------------
+
+	private void ClicouBotaoTrocaPet(object sender, EventArgs args) 
+	{
+
+	}
+
+//------------------------------------------------------------
+
+	private void ClicouBotaoComida(object sender, EventArgs args) 
+	{
+		
+	}
+
+//------------------------------------------------------------
+
+	private void ClicouBotaoAgua(object sender, EventArgs args) 
+	{
+		
+	}
+
+//------------------------------------------------------------
+
+	private void ClicouBotaoBrincar(object sender, EventArgs args) 
+	{
 		
 	}
 }
