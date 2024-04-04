118f3b77f4d4589b9b689356bcb8a2cf35f28946
Erick <erickfrias8006@gmail.com>
2024-04-04 11:39:36 -0300

    CABEI!

--- PouBeLike/MainPage.xaml.cs
+++ PouBeLike/MainPage.xaml.cs
@@ -10,19 +10,60 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-		
+	
+//------------------------------------------------------------
+
+		var timer = Application.Current.Dispatcher.CreateTimer();
+		timer.Interval = TimeSpan.FromSeconds(1);
+		timer.Tick += (s, e) => PassouTempo();
+		timer.Start();
+
+//------------------------------------------------------------
+
 		SirCapivaldo = new Capybara();
 		Orengotengo = new Orangotango();
 		Douglas = new Duck();
+
+//------------------------------------------------------------
+
 
 		Atual = SirCapivaldo;
 		ImagemAnimal.Source = Atual.GetArquivo();
 		LabelNome.Text = Atual.GetNomePersonagem();
 
+//------------------------------------------------------------
+
+
 		Atual.SetSede(0.5);
 		Atual.SetFome(0.5);
 		Atual.SetFelicidade(0.5);
 
+	}
+
+//------------------------------------------------------------
+
+	void PassouTempo()
+	{
+		Atual.SetFome(Atual.GetFome() - 0.1);
+		BarraFome.Progress = Atual.GetFome();
+
+		Atual.SetSede(Atual.GetSede() - 0.1);
+		BarraSede.Progress = Atual.GetSede();
+
+		Atual.SetFelicidade(Atual.GetFelicidade() - 0.1);
+		BarraFelicidade.Progress = Atual.GetFelicidade();
+
+		SirCapivaldo.SetFome(SirCapivaldo.GetFome() - 0.1);
+		SirCapivaldo.SetSede(SirCapivaldo.GetSede() - 0.1);
+		SirCapivaldo.SetFelicidade(SirCapivaldo.GetFelicidade() - 0.1);
+
+		Orengotengo.SetFome(Orengotengo.GetFome() - 0.1);
+		Orengotengo.SetSede(Orengotengo.GetSede() - 0.1);
+		Orengotengo.SetFelicidade(Orengotengo.GetFelicidade() - 0.1);
+
+		Douglas.SetFome(Douglas.GetFome() - 0.1);
+		Douglas.SetSede(Douglas.GetSede() - 0.1);
+		Douglas.SetFelicidade(Douglas.GetFelicidade() - 0.1);
 	}
 
 //------------------------------------------------------------
@@ -55,6 +96,9 @@ public partial class MainPage : ContentPage
 		
 		ImagemAnimal.Source = Atual.GetArquivo();
 		LabelNome.Text = Atual.GetNomePersonagem();
+		BarraFome.Progress = Atual.GetFome();
+		BarraSede.Progress = Atual.GetSede();
+		BarraFelicidade.Progress = Atual.GetFelicidade();
 	}
 
 //------------------------------------------------------------
